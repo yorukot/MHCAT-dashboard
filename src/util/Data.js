3a210c2a82f1e88043be71cb61792ad634bcfbf1
Yorukot <ben.hrc57@gmail.com>
2024-06-13 21:35:11 +0800

    update url

--- src/util/Data.js
+++ src/util/Data.js
@@ -1,4 +1,4 @@
 export const DISCORD_API_URL = 'https://discord.com/api/v10'
-export const MHCATAPIURL = 'https://mhcat.xyz'
+export const MHCATAPIURL = 'https://mhcat.nightcat.xyz'
 //http://localhost:3000
 //https://mhcat.xyz
