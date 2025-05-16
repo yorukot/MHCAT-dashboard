ecd06d61a282c9299a66ea6aa348c5cb82684c84
Yorukot <hi@yorukot.me>
2025-05-16 23:57:00 +0800

    feat: update MHCAT api endpoint

--- src/util/Data.js
+++ src/util/Data.js
@@ -1,4 +1,4 @@
 export const DISCORD_API_URL = 'https://discord.com/api/v10'
-export const MHCATAPIURL = 'https://mhcat.nightcat.xyz'
+export const MHCATAPIURL = 'https://mhcat.yorukot.me'
 //http://localhost:3000
 //https://mhcat.xyz
