10d7b174507e72da5727e765cea60478e74fe848
Yorukot <hi@yorukot.me>
2025-09-05 16:53:44 +0800

    fix: fix some build error

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
-require('dotenv').config()
-
-const mongoose = require("mongoose")
-
-try {
-  mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      autoIndex: false
-  }).then(console.log('Connected to mongodb'))
-} catch (error) {
-  console.error(error);
-}
 
 const nextConfig = {
   reactStrictMode: true,
 }
-console.log(process.env.NODE_ENV)
+
 module.exports = nextConfig
